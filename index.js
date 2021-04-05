59ff26487d1b771a3450de1dab944096c570683d
iii <miselrkba@gmail.com>
2021-04-05 13:24:29 +0200

    setup server to serve malconf output file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const fs = require('fs');
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  fs.readFile('output.txt', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.send(data);
+  });
+});
+
+app.listen(4000);
